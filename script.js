a2a8d4ed438b52e2df1a6007ff243f3d88aa6a65
akiwoods <ahmad.woods@braze.com>
2024-11-14 10:45:42 -0500

    move banner function to script.js file

--- script.js
+++ script.js
@@ -3,6 +3,24 @@
 var userId, firstName, lastName, email, selectedGender, birthday, country, homeCity, phoneNumber, attributeName, attributeValue, eventName, eventProperties, productId, currencyCode, price, quantity;
 
 
+//Banner Handling
+braze.requestBannersRefresh(["akiplace"])
+  // get this placement's banner. If it's `null` the user did not qualify for one.
+const globalBanner = braze.getBanner("akiplace");
+  
+  // choose where in the DOM you want to insert the banner HTML
+const container = document.getElementById("aki-hero");
+  
+  // Insert the banner which replacees the innerHTML of that container
+braze.insertBanner(globalBanner, container);
+  
+  // Special handling if the user is part of a Control Variant
+if (globalBanner.isControl) {
+      // hide or collapse the container
+      container.style.display = 'none';
+  }
+
+
 //THIS CODE SNIPPET (WHICH IS PRESENT WITH EVERY FUNCTION) ALLOWS FOR CLICKING 'ENTER' TO SUBMIT, NO NEED TO TOUCH IT
 var userr = document.getElementById("change_user_text");
 userr.addEventListener("keyup", function(event){
